9bb6b8fa86babcc8cb149b1bc661cdf203187a3f
updown-90 <updown_90@kakao.com>
2021-10-07 13:38:57 +0900

    feat: 프로젝트 세팅 및 Security 설정 및 AccountController 개발

---
+++ src/main/java/com/updown/domain/Account.java
@@ -0,0 +1,55 @@
+package com.updown.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickName;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime createdDate;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
---
+++ src/test/java/com/updown/UpDownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
