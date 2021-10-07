a0f6e6290a7052e4aebc7868d0d86a600ade601a
updown-90 <updown_90@kakao.com>
2021-10-08 00:08:18 +0900

    feat: 회원가입 화면 개발

---
+++ src/main/java/com/updown/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package com.updown.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpForm {
+    private String nickname;
+    private String email;
+    private String password;
+}
