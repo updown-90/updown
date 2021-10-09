9873557fb0f158ea9f7692ce2c907f2ab79db3d7
updown-90 <updown_90@kakao.com>
2021-10-09 11:00:00 +0900

    feat: 회원가입 폼 서브밋 검증 로직 추가

--- src/main/java/com/updown/account/AccountController.java
+++ src/main/java/com/updown/account/AccountController.java
@@ -1,11 +1,26 @@
 package com.updown.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -13,4 +28,16 @@ public class AccountController {
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        signUpFormValidator.validate(signUpForm, errors);
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/com/updown/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.updown.account;
+
+import com.updown.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsBynickname(String nickname);
+}
